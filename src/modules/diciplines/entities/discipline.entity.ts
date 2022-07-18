a5becd72e8e688740f262800b93641b52c2207aa
Ulguim <williamulguim2014@gmail.com>
2022-07-18 08:51:34 -0300

    :sparkles: Adicionado Relacionamentos Many to Many
    Adicionado Relacionamento Many To Many entre Tabelas Students e Diciplines

--- src/modules/diciplines/entities/discipline.entity.ts
+++ src/modules/diciplines/entities/discipline.entity.ts
@@ -1,9 +1,14 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Base } from 'src/modules/base/entities/base.entity';
-import { Column ,Entity} from 'typeorm';
-
+import { Column ,Entity, JoinTable} from 'typeorm';
+import { Student } from 'src/modules/students/entities/student.entity';
+import { ManyToMany } from 'typeorm';
 @Entity()
 export class Discipline  extends Base{
   @Column()
   name:string;
+
+  @ManyToMany(()=>Student, (students)=> students.disciplines,)
+   @JoinTable()
+  students:Student[];
 }
--- src/modules/students/dto/student.dto.ts
+++ src/modules/students/dto/student.dto.ts
@@ -1,9 +1,11 @@
-import { FilterableField } from '@nestjs-query/query-graphql';
+import { FilterableOffsetConnection, FilterableField } from '@nestjs-query/query-graphql';
 import { ObjectType, Field, Int, InputType } from '@nestjs/graphql';
 import { BaseDTO } from 'src/modules/base/dto/base.dto';
+import { DisciplineDTO } from 'src/modules/diciplines/dto/discipline.dto';
 import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType('student')
+@ObjectType('Student')
+@FilterableOffsetConnection('disciplines',()=>DisciplineDTO,{nullable:true})
 export class StudentDTO  extends BaseDTO{
   
 
